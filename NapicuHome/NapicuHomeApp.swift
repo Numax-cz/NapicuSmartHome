0fb22658da5338089035ee627f65367e9dd7e017
Marcel <numax.cz@protonmail.ch>
2024-10-15 14:17:11 +0200

    Initial Commit

---
+++ NapicuHome/NapicuHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NapicuHomeApp.swift
+//  NapicuHome
+//
+//  Created by Marcel Mikoláš on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct NapicuHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
